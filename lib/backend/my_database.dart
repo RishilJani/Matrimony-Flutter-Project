943739c4c6640503bfd9177a39580bddfef30a95
RishilJani <rishilj9108@gmail.com>
2025-02-10 17:56:38 +0530

    Setting Database

---
+++ lib/backend/my_database.dart
@@ -0,0 +1,32 @@
+import 'package:matrimony_application/utils/string_constants.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class MyDatabase {
+  Future<Database> initDatabase() async {
+    print("Opening database");
+    Database db = await openDatabase(
+            join(await getDatabasesPath(), 'myDatabase.db'),
+            onCreate: (db, version)  async {
+             await db.execute('''
+                CREATE TABLE IF NOT EXISTS $TABLE ( 
+                  $UserId INTEGER PRIMARY KEY AUTOINCREMENT,
+                  $Name TEXT NOT NULL,
+                  $Email TEXT NOT NULL,
+                  $Mobile TEXT NOT NULL,
+                  $DOB TEXT NOT NULL,
+                  $City TEXT NOT NULL,
+                  $Gender TEXT NOT NULL,
+                  $Password TEXT NOT NULL,
+                  $Hobbies TEXT,
+                  $isFavourite INTEGER NOT NULL);
+                  ''');
+             },
+            version: DB_Vesion
+        );
+    print("Returning database");
+    return db;
+  }
+
+
+}
